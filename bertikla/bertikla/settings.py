eec4716d9f50a55a6588ae6660aa88d6d1de9fa6
Celil Akdemir <celilakdemir@trair.com.tr>
2024-04-11 17:19:48 +0300

    please deploy...

--- bertikla/bertikla/settings.py
+++ bertikla/bertikla/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p254(mp744i196r&w874fn&8+_od7si*t2)b&7uzgm3w04@a-r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
