38edd590a30437465a1aa1878059c53ca29d9c99
Celil Akdemir <celilakdemir@trair.com.tr>
2024-04-12 16:42:41 +0300

    please work :(

--- bertikla/bertikla/settings.py
+++ bertikla/bertikla/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = ['160.20.110.222:8000',
                  '160.20.110.222',
                  'localhost',
                  '127.0.0.1',
-                 'berticoffee.com']
+                 'berticoffee.com',
+                 'www.berticoffee.com']
 
 
 # Application definition
