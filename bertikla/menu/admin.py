5783a42ea978c973838863b619c248d0758a170b
Celil Akdemir <celilakdemir@trair.com.tr>
2024-04-29 03:09:23 +0300

    admin page filter and search

--- bertikla/menu/admin.py
+++ bertikla/menu/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from .models import Categories, Products
+from .models import *
 
-admin.site.register(Categories)
-admin.site.register(Products)
+
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category_id', 'price')
-    pass
+    search_fields = ('name', 'price')
+
+admin.site.register(Categories)
+admin.site.register(Products, ProductAdmin)
